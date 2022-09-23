971bb5feedf6312907346ef9bf64fbdd7d0c1b5f
Joel Campos <joelcamposoliva9000@gmail.com>
2022-09-23 22:14:58 +0200

    Logout

--- app/src/view/dashboard/DashboardViewPage.tsx
+++ app/src/view/dashboard/DashboardViewPage.tsx
@@ -1,13 +1,27 @@
-import { Col, Row } from "antd";
+import { Button, Card, Col, Row } from "antd";
 import Container from "../../components/UI/Container";
+import { useAuth } from "../../providers/authentication/AuthenticationProvider";
 import MyAccounts from "./my-accounts/MyAccounts";
 
 export default function DashboardViewPage() {
+
+    const { setAuthContext } = useAuth();
+
     return (
         <Container>
             <Row
                 gutter={[12, 12]}
             >
+                <Col
+                    span={24}
+                >
+                    <Card>
+                        <Button
+                            type="primary"
+                            onClick={() => setAuthContext(undefined)}
+                        >Exit account</Button>
+                    </Card>
+                </Col>
                 <Col
                     xs={24}
                 >
